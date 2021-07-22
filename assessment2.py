6e705eccf157e04f149b03a2ca8f803c229fc189
Furqan Ahmed <Fahmed1519@gmail.com>
2021-07-23 01:44:25 +0530

    Changes in Feature-Z

--- assessment2.py
+++ assessment2.py
@@ -6,13 +6,13 @@ assessment3 = int(input("Enter the Assessment 3 Marks:"))
 
 total_marks = (assessment1 * 20 + assessment2 * 40 + assessment3 *40)/100 
 
-if total_marks > 85 and total_marks <= 100:
+if total_marks > 90 and total_marks <= 100:
     print("HD")
-elif total_marks > 75 and total_marks <=85:
+elif total_marks > 70 and total_marks <=90:
     print("D")
-elif total_marks > 65 and total_marks <=75:
+elif total_marks > 60 and total_marks <=70:
     print("C")
-elif total_marks >= 50 and total_marks <= 65:
+elif total_marks >= 50 and total_marks <= 60:
     print("P")
 else:
     print("F")
