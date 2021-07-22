b787d5f445f7daa6e318fd18f45b5fb683901245
Furqan Ahmed <Fahmed1519@gmail.com>
2021-07-23 01:37:10 +0530

    Changes in Feature-X

--- assessment2.py
+++ assessment2.py
@@ -6,13 +6,16 @@ assessment3 = int(input("Enter the Assessment 3 Marks:"))
 
 total_marks = (assessment1 * 20 + assessment2 * 40 + assessment3 *40)/100 
 
+grade = "F"
 if total_marks > 85 and total_marks <= 100:
-    print("HD")
+    grade = "HD"
 elif total_marks > 75 and total_marks <=85:
-    print("D")
+    grade = "D"
 elif total_marks > 65 and total_marks <=75:
-    print("C")
+    grade = "C"
 elif total_marks >= 50 and total_marks <= 65:
-    print("P")
+    grade = "P"
 else:
-    print("F")
+    grade = "F"
+
+print(grade)
